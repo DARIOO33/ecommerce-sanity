84602962e59ed9f8dbc3af81c6968dcdbdf2a0be
DARIOO33 <darioanwar3@gmail.com>
2023-09-19 19:53:18 +0100

    test

--- src/schemas/post.js
+++ src/schemas/post.js
@@ -90,6 +90,7 @@ const post = {
                     { title: '10%', value: '10%' },
                     { title: '15%', value: '15%' },
                     { title: '20%', value: '20%' },
+                    { title: '25%', value: '25%' },
                     { title: '30%', value: '30%' },
                     { title: '40%', value: '40%' },
                     { title: '50%', value: '50%' },
