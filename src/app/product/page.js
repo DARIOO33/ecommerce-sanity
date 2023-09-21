75515f307b8c72ee591e64d405f2eac498575e1f
DARIOO33 <darioanwar3@gmail.com>
2023-09-21 13:08:49 +0100

    small update

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,8 +1,23 @@
-export default function page(params) {
-    console.log(params);
+import { getPosts } from "@/sanity/sanity.query";
+
+export default async function page(params) {
+    const products = await getPosts();
+    const postId = (params.searchParams.id);
+    const currectProduct = products.filter((product => product._id == postId))
+
+    console.log(currectProduct);
     return (
         <div>
+            {currectProduct.map((product => (
+                <div key={product._id} className="product-container">
+                    <h1> id={product._id}</h1>
+                    <h1> {product.product_name}</h1>
 
+
+                </div>
+
+
+            )))}
         </div>
     )
 };
