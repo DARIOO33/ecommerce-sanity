e2d08f5b59f35d151e2d8cb23ffe781e9ab3a40b
DARIOO33 <darioanwar3@gmail.com>
2023-09-21 21:50:01 +0100

    updates for laptop !

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,17 +1,63 @@
 import { getPosts } from "@/sanity/sanity.query";
+import "./product.css"
+import Tag from "@/app/components/Tag"
 
 export default async function page(params) {
     const products = await getPosts();
     const postId = (params.searchParams.id);
     const currectProduct = products.filter((product => product._id == postId))
+    function toNumber(str) {
+        let discountNumber;
+
+        str.length > 2 ? discountNumber = str.substr(0, 2) : discountNumber = str.substr(0, 1)
+        return discountNumber
+    }
 
     console.log(currectProduct);
     return (
         <div>
             {currectProduct.map((product => (
                 <div key={product._id} className="product-container">
-                    <h1> id={product._id}</h1>
-                    <h1> {product.product_name}</h1>
+                    <div className="product-images">
+                        <div className="product-img">
+                            <img src={product.imageLink.image1} />
+                        </div>
+                        <div className="images-c flex">
+                            <img src={product.imageLink.image1} />
+                            <img src={product.imageLink.image2} />
+                        </div>
+                    </div>
+                    <div className="pr-details">
+                        <h1 className="text-3xl font-bold">{product.product_name}</h1>
+                        <div className="mt-2">
+
+                            {product.discount.length > 1
+                                ?
+                                <Tag text={"Save"} bg={"red"} percent={toNumber(product.discount)} />
+
+                                :
+                                <div></div>
+                            }
+                        </div>
+                        <p className=" font-semibold mt-2 text-black">{product.product_desription}</p>
+                        <div className="flex items-center justify-evenly mt-4">
+                            <div>
+
+                                <div className="price laptop:text-2xl mt-1 mobile:text-sm font-bold">
+                                    ${(product.product_price).toFixed(2)}
+                                </div>
+                                <div className="shipping text-lg mt-1 text-blue-800 font-bold">
+                                    FREE SHIPPING
+                                </div>
+                            </div>
+                            <div className="buttonn">
+                                <button className="rounded-full laptop:text-2xl mobile:text-xs  px-4 py-1 font-semibold slide text-blue-800 cursor-pointer">
+                                    Buy Now
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
 
 
                 </div>
